f2a4aaee692c790cc28f3115705f3a7053266431
Abdul Kader <abdulkader0126@gmail.com>
2024-12-03 18:00:55 +0700

    Update Footer.jsx

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -62,14 +62,14 @@ const Footer = () => {
                             <div className="footer__widget">
                                 <h4 className="footer__widget-title">Maps</h4>
                                 <div className="footer__contact-content">
-                                    <iframe
+                                    {/* <iframe
                                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63219.15529079024!2d112.59058342461867!3d-7.978558312256583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd62822063dc2fb%3A0x78879446481a4da2!2sMalang%2C%20Kota%20Malang%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1732950851150!5m2!1sid!2sid"
                                         width="450"
                                         height="250"
                                         style={{ border: "0" }}
                                         allowFullScreen=""
                                         loading="lazy"
-                                    />
+                                    /> */}
                                 </div>
                             </div>
                         </div>
