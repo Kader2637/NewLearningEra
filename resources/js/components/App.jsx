7d34ee73b5f50e393413d8ae2ae82aced4a0fee7
Abdul Kader <abdulkader0126@gmail.com>
2024-12-03 17:59:12 +0700

    Update App.jsx

--- resources/js/components/App.jsx
+++ resources/js/components/App.jsx
@@ -18,6 +18,69 @@ const App = () => {
     return (
         <div>
             <header>
+                {/* <div className="tg-header__top">
+                    <div className="container custom-container">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <ul className="tg-header__top-info list-wrap">
+                                    <li>
+                                        <img
+                                            src="http://127.0.0.1:8000/assets/img/icons/map_marker.svg"
+                                            alt="Icon"
+                                        />
+                                        <span>Kota Malang, Jawa Timur</span>
+                                    </li>
+                                    <li>
+                                        <img
+                                            src="assets/img/icons/envelope.svg"
+                                            alt="Icon"
+                                        />
+                                        <a href="mailto:info@skillgrodemo.com">
+                                            TrioOfInnovator@gmail.com
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-6">
+                                <div className="tg-header__top-right">
+                                    <div className="tg-header__phone">
+                                        <img
+                                            src="assets/img/icons/phone.svg"
+                                            alt="Icon"
+                                        />
+                                        Hubungi:{" "}
+                                        <a href="0987654321">+62 0987654321</a>
+                                    </div>
+                                    <ul className="tg-header__top-social list-wrap">
+                                        <li>Follow Us On:</li>
+                                        <li>
+                                            <a href="#">
+                                                <i className="fab fa-facebook-f"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i className="fab fa-twitter"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i className="fab fa-whatsapp"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <i className="fab fa-linkedin-in"></i>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+ */}
+
                 <div id="header-fixed-height"></div>
                 <div id="sticky-header" className="tg-header__area">
                     <div className="container custom-container">
